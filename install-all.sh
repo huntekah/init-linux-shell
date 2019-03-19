9d675ca52ced7257bf67564eacf5be97e88f8efd
Grzegorz Miejski <grz.miejski@gmail.com>
2019-03-19 12:05:02 +0100

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,3 +1,11 @@
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 source ~/.bashrc
+
+# brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
